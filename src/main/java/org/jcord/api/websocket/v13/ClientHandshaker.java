aef810f2aa112c644271df9055b7ef02e9c05932
KoblizekXD <janprokupek@centrum.cz>
2023-05-19 19:21:55 +0200

    created: Handshaker utility creation methods

--- src/main/java/org/jcord/api/websocket/v13/ClientHandshaker.java
+++ src/main/java/org/jcord/api/websocket/v13/ClientHandshaker.java
@@ -1,5 +1,6 @@
 package org.jcord.api.websocket.v13;
 
+import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker13;
@@ -8,8 +9,22 @@ import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 import java.net.URI;
 
 import static io.netty.handler.codec.http.HttpHeaderNames.ORIGIN;
+import static io.netty.handler.codec.http.websocketx.WebSocketVersion.V13;
 
 public class ClientHandshaker extends WebSocketClientHandshaker13 {
+    public static ClientHandshaker newHandshaker(URI uri, String protocol, boolean allowExtensions, HttpHeaders headers) {
+        return new ClientHandshaker(
+                uri, V13, protocol, allowExtensions, headers, 65536,true,
+                false,-1
+        );
+    }
+    public static ClientHandshaker newHandshaker(URI uri) {
+        return new ClientHandshaker(
+                uri, V13, null, true, new DefaultHttpHeaders(), 65536,true,
+                false,-1
+        );
+    }
+
     public ClientHandshaker(URI webSocketURL, WebSocketVersion version, String subprotocol, boolean allowExtensions, HttpHeaders customHeaders, int maxFramePayloadLength) {
         super(webSocketURL, version, subprotocol, allowExtensions, customHeaders, maxFramePayloadLength);
     }
