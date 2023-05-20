eb007f8cba571b41da35786d5740cfb8260155bb
KoblizekXD <janprokupek@centrum.cz>
2023-05-20 08:35:36 +0200

    done: handling incoming stuff
    Took 5 minutes

--- src/main/java/org/jcord/api/websocket/v13/SocketHandler.java
+++ src/main/java/org/jcord/api/websocket/v13/SocketHandler.java
@@ -1,12 +1,14 @@
 package org.jcord.api.websocket.v13;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakeException;
-import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
 import org.jcord.api.websocket.Gateway;
 
 public class SocketHandler extends SimpleChannelInboundHandler<Object> {
@@ -32,6 +34,11 @@ public class SocketHandler extends SimpleChannelInboundHandler<Object> {
     public void channelInactive(ChannelHandlerContext ctx) {
         Gateway.LOGGER.warn("Channel closure detected");
     }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        Gateway.LOGGER.error("An unknown exception was thrown: ", cause);
+        ctx.close();
+    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -47,6 +54,17 @@ public class SocketHandler extends SimpleChannelInboundHandler<Object> {
             }
             return;
         }
-        // TODO: Finish data obtain
+        if (msg instanceof FullHttpResponse response) {
+            Gateway.LOGGER.error("Unexpected fatal error: ", new IllegalStateException(
+                    "Unexpected Http Response (status = " + response.status() +
+                            ", content = " + response.content().toString(CharsetUtil.UTF_8) + ')'));
+        }
+        WebSocketFrame frame = (WebSocketFrame) msg;
+        if (frame instanceof TextWebSocketFrame textFrame) {
+            String text = textFrame.text();
+        } else if (frame instanceof CloseWebSocketFrame) {
+            Gateway.LOGGER.warn("WebSocket requested closure, disconnecting...");
+            channel.close();
+        }
     }
 }
