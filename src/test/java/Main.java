5cfa61293b14c8bfcdefb13c2ca883f333766abf
KoblizekXD <janprokupek@centrum.cz>
2023-05-20 14:43:15 +0200

    added: jitpack yml
    Took 51 seconds

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,4 @@
-import com.google.gson.Gson;
-import org.jcord.api.util.Payload;
 import org.jcord.api.websocket.Gateway;
-import org.jcord.api.websocket.identify.IdentifyJson;
 
 public class Main {
     public static void main(String[] args) {
